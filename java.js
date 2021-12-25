c37a16d5b5c91346060edfdc12bc78fc57ed1c89
Sushilkana <86962755+Sushilkana@users.noreply.github.com>
2021-12-25 15:55:25 +0530

    Add files via upload

--- java.js
+++ java.js
@@ -2,20 +2,20 @@ Webcam.set({
     height:290,
     width:290,
     Image_format:"png",
-    pang_quality:100
+    pang_quality:90
 });
 
 camera = document.getElementById("div_cam");
 Webcam.attach("#div_cam");
-
-console.log(ml5.version);
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/train/image/1nLzGJkNfWEVGbR087rib3BJIh7MLMgnX', model_loaded)
 
 function Get_snapshot(){
     Webcam.snap(function(data_uri){
         document.getElementById("div_cap").innerHTML = "<img id='output_img' src="+data_uri+">"
     })
 }
+
+console.log(ml5.version);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/hxHJoHcUq/model.json', model_loaded);
 
 function model_loaded() {
     console.log("model is loaded now...");
